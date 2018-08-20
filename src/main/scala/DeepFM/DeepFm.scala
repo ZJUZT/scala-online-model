e6f4dd41fc8f832d625be7199f68d1ba074238fd
ZJUZT <zhangteng398@gmail.com>
2018-08-20 09:17:52 +0800

    ...

---
+++ src/main/scala/DeepFM/DeepFm.scala
@@ -0,0 +1,137 @@
+
+package DeepFM
+
+import scala.Array._
+/**
+  *
+  * init trained deep_fm model
+  * @param anchor the anchor point
+  * @param fm_embedding fm second embedding
+  * @param input_layer_weight deep part input layer weights
+  * @param hidden_layer_weight deep part hidden layer weights
+  */
+class DeepFm (anchor:Array[Double],
+              fm_embedding:Array[Array[Array[Double]]],
+              fm_bias: Double,
+              input_layer_weight:Array[Array[Double]],
+              input_layer_bias:Array[Double],
+              hidden_layer_weight:Array[Array[Double]],
+              hidden_layer_bias:Array[Double]) {
+//  var this.anchor: Array[Double] = anchor
+//  var this.fm_embedding:Array[Array[Array[Double]]] = fm_embedding
+//  var this.fm_bias: Array[Double] = fm_bias
+//  var this.input_layer:Array[Array[Double]] = input_layer
+//  var this.input_layer_bias:Array[Double] = input_layer_bias
+//  var this.hidden_layer:Array[Array[Double]] = hidden_layer
+//  var this.hidden_layer_bias:Array[Double] = hidden_layer_bias
+
+
+  /**
+    * predict model
+    * @param xi non-zero index for each field
+    * @param xv corresponding value
+    * @return predict score
+    */
+  def predict(xi:Array[Array[Int]], xv:Array[Array[Double]]): Double = {
+
+    /*
+    calculate fm embedding
+     */
+
+    val fields_num = 19
+    val embedding_size = 4
+    var fm_second_order_embedding = ofDim[Double](fields_num, embedding_size)
+
+    for (i <- 0 to fm_embedding.length){
+      val xi_field = xi(i)
+      val xv_field = xv(i)
+
+      for (j <- xi_field.indices){
+        for (k <- 0 until embedding_size){
+          fm_second_order_embedding(i)(j) = fm_second_order_embedding(i)(j) +
+            fm_embedding(i)(xi_field(j))(k) * xv_field(j)
+        }
+      }
+    }
+
+    /*
+    fm part output
+    use 2xy = (x+y)^2 - x^2 - y^2 as short path
+     */
+
+    var fm_square_sum = 0.0
+    var fm_sum_square = 0.0
+
+
+    for (i <- 0 to embedding_size){
+      var tmp = 0.0
+      for (j <- 0 to fm_embedding.length){
+        fm_square_sum = fm_square_sum + (fm_second_order_embedding(j)(i) * fm_second_order_embedding(j)(i))
+        tmp += fm_second_order_embedding(j)(i)
+      }
+      fm_sum_square = fm_square_sum + tmp * tmp
+    }
+
+    var fm_output = (fm_sum_square - fm_square_sum) * 0.5 + fm_bias
+
+    /*
+    concat field fm embedding as deep part input
+     */
+    var deep_input = Array[Double](fields_num * embedding_size)
+    for (i <- fm_embedding.indices){
+      for (j <-0 until embedding_size){
+        deep_input(i * embedding_size + j) = fm_second_order_embedding(i)(j)
+      }
+    }
+
+    /*
+    calculate deep part
+     */
+
+    val fm_width = fields_num * embedding_size
+    val input_width = input_layer_bias.length
+    val hidden_width = hidden_layer_bias.length
+
+    // input to hidden
+    var input_layer_output = new Array[Double](input_width)
+    for (i <-0 until input_width){
+      for (j <- 0 until fm_width){
+        input_layer_output(i) = deep_input(j) * input_layer_weight(j)(i)
+      }
+      input_layer_output(i) += input_layer_bias(i)
+      // relu
+      if(input_layer_output(i) < 0){
+        input_layer_output(i) = 0
+      }
+    }
+
+    var hidden_layer_output = new Array[Double](hidden_width)
+
+    // hidden to output
+    for (i <- 0 until hidden_width){
+      for (j <- 0 until input_width){
+        hidden_layer_output(i) = input_layer_output(j) * hidden_layer_weight(j)(i)
+      }
+      hidden_layer_output(i) += hidden_layer_bias(i)
+
+      // relu
+      if(hidden_layer_output(i) < 0){
+        hidden_layer_output(i) = 0
+      }
+    }
+
+    // deep output
+    var deep_output = 0.0
+    for (i <- 0 until hidden_width){
+      deep_output = deep_output + hidden_layer_output(i)
+    }
+
+    // sum fm and deep output
+    // then sigmoid
+
+    var predict_prob = 1/(1+math.exp(-(fm_output + deep_output)))
+
+    return predict_prob
+
+  }
+}
--- src/main/scala/predict.scala
+++ src/main/scala/predict.scala
@@ -4,6 +4,12 @@
 
 import org.apache.spark.rdd.RDD
 
+import scala.io._
+import DeepFM.DeepFm
+import scala.Array._
+
+import scala.collection.mutable.ArrayBuffer
+
 /**
   * Feature format
   * xi: non-zero index
@@ -21,24 +27,87 @@ object predict {
 
   def main(args: Array[String]): Unit = {
     // num of anchor points
-    var num_anchor = 20
+    val num_anchor = 20
 
     // num of nearest neighbours
-    var num_nn = 3
+    val num_nn = 3
 
     // deep layer
-    var deep_layer = 2
+    val input_width = 32
+
+    val hidden_width = 32
 
     // feature number
-    var num_feature = 1935
+    val num_feature = 1935
+
+    val c = 0.01
+    val embedding_size = 4
 
     // field dict
-    var field_info = Array(29, 33, 34, 35, 51, 201, 351, 501, 651, 801, 951, 1101, 1251, 1301, 1601, 1636, 1731, 1801, 1950)
+    val field_info = Array(29, 33, 34, 35, 51, 201, 351, 501, 651, 801, 951, 1101, 1251, 1301, 1601, 1636, 1731, 1801, 1950)
 
-    // load fm embedding and nn for each anchor point
-    var tmp: Array[Array[Int]] = Array(Array(1,2,3))
-    tmp = tmp :+ Array(2,3)
+    // fm embedding length
+    /*
+    load fm embedding and nn for each anchor point
+    load from table online
+    mock by load from file offline
+     */
 
-    print(tmp(1)(0))
+    val table_name = new Array[String](num_anchor)
+    for (i <- 0 until num_anchor){
+      table_name(i) = "data/deep_fm_" + i
+    }
+
+    for (i <- table_name.indices){
+      var lines = Source.fromFile(table_name(i)).getLines()
+      // anchor point
+      var anchor_point = lines.next()
+      var tokens = anchor_point.split(" ")
+      var ap = new Array[Double](tokens.length)
+      for (i <- ap.indices){
+        ap(i) = tokens(i).toDouble
+      }
+
+      // fm bias
+      var fm_bias = lines.next().toDouble
+
+      // fm embedding
+      var fm_embedding = new ArrayBuffer[Any]()
+      for (i <- field_info.indices){
+        var rows = 0
+        if (i==0){
+          rows = field_info(i) + 1
+        }
+        else{
+          rows = field_info(i) - field_info(i-1)
+        }
+
+        var value = lines.next().split(" ")
+        var field_embedding = ofDim[Double](rows, embedding_size)
+
+        for (i <- value.indices) {
+          field_embedding(i / embedding_size)(i % embedding_size) = value(i).toDouble
+        }
+
+        fm_embedding = fm_embedding ++ field_embedding
+
+      }
+
+      // input layer weight
+      var input_layer_weight = ofDim[Double](field_info.length * embedding_size)
+
+      // input layer bias
+      var input_layer_bias = new Array[Double](input_width)
+
+      // hidden layer weight
+      var hidden_layer_weight = ofDim[Double](input_width, hidden_width)
+
+      // hidden layer bias
+      var hidden_layer_bias = new Array[Double](hidden_width)
+
+
+    }
+
+
   }
 }
